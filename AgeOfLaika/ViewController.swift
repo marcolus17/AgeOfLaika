6282b20a27b014199d3e087e455f93842152cc2a
Nick Markworth <marcolus17@users.noreply.github.com>
2015-02-24 20:06:43 -0600

    AgeOfLaika Initial Commit
    Initial commit. Has all changes and challenges. Convert from human
    years to dog years.

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var humanYearsLabel: UILabel!
+    @IBOutlet weak var ageErrorLabel: UILabel!
+    @IBOutlet weak var dogAgeTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,42 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    @IBAction func convertHumanAgeToDogYears(sender: UIButton) {
+        // Check to see if text field is empty or only contains whitespaces
+        let whitespaceSet = NSCharacterSet.whitespaceAndNewlineCharacterSet()
+        if dogAgeTextField.text.stringByTrimmingCharactersInSet(whitespaceSet) != "" {
+            // Check to see if string converted to Int properly
+            let numberFromTextField: Int? = dogAgeTextField.text.toInt()
+            if numberFromTextField != nil {
+                // Get Int value for age and convert to dog years
+                ageErrorLabel.hidden = true
+                let dogAge = Double(numberFromTextField!)
+                var humanAge:Double = 0
+                if dogAge > 2 {
+                    humanAge = (2 * 10.5) + (4 * (dogAge - 2))
+                }
+                else {
+                    humanAge = dogAge * 10.5
+                }
+                humanYearsLabel.hidden = false
+                humanYearsLabel.text = "\(humanAge)"
+            }
+            else {
+                // Show error
+                ageErrorLabel.hidden = false
+                humanYearsLabel.hidden = true
+            }
+        }
+        else {
+            // Show error
+            ageErrorLabel.hidden = false
+            humanYearsLabel.hidden = true
+        }
+        
+        dogAgeTextField.text = ""
+        dogAgeTextField.resignFirstResponder()
+    }
 }
 
